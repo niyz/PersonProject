889f006e3f8195380d828b84adab94b008cf9d83
Per-Anders Karlsson <karlsson.peranders@gmail.com>
2019-09-23 14:01:30 +0200

    Add: Empty functions to handle sql queries

---
+++ src/se/experis/DataBase.java
@@ -0,0 +1,32 @@
+package se.experis;
+
+import java.sql.Connection;
+
+public class DataBase {
+    public DataBase(){
+
+    }
+
+    public Connection dbConnect(){
+
+        return null;
+    }
+
+    public void dbCreate(){ // booleand?
+
+    }
+
+    public void dbRead(String searchStr){
+
+    }
+
+    public boolean dbUpdate(String sqlStmt){
+
+        return false;
+    }
+
+    public boolean dbDelete(String sqlStmt){
+
+        return false;
+    }
+}
