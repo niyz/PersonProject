bc7ad0b9342e77db4e69814ad93deaf1d2d1b47a
Maria <mariajohnsson89@gmail.com>
2019-09-23 14:31:45 +0200

    Create Address.java

---
+++ src/se/experis/Address.java
@@ -0,0 +1,9 @@
+package se.experis;
+
+public class Address {
+    public String country;
+    public String city;
+    public String street;
+    public String streetNum;
+    public String postalCode;
+}
