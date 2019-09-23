eecb441e7cedc3f5891e9cc30a2f1917e083480f
Maria <mariajohnsson89@gmail.com>
2019-09-23 14:43:44 +0200

    Update Address.java

--- src/se/experis/Address.java
+++ src/se/experis/Address.java
@@ -1,9 +1,21 @@
 package se.experis;
 
 public class Address {
-    public String country;
-    public String city;
-    public String street;
-    public String streetNum;
-    public String postalCode;
+    private String country;
+    private String city;
+    private String street;
+    private String streetNum;
+    private String postalCode;
+
+    Address (String country, String city, String street, String streetNum, String postalCode) {
+        this.country = country;
+        this.city = city;
+        this.street = street;
+        this.streetNum = streetNum;
+        this.postalCode = postalCode;
+    }
+
+    public void printAddress() {
+        System.out.println("Country:" + country + "City:" + city + "Street:" + street + "Street number:" +  streetNum + "Postal code:" + postalCode);
+    }
 }
